23f54650d9342f1dc7c42cbc13db7c59492f82db
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-11-28 01:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
-  const arr = []
+  var arr = []
   for (let i = 0, l = musicians.length; i < l; i++) {
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  const shoutedFacts = []
+  var shoutedFacts = []
   let i = 0
   while (i < facts.length) {
     shoutedFacts.push(`${facts[i]}!!!`)
