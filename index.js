03bb363f9a41c2009e57804225550180573ed859
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-11-28 01:04:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   const shoutedFacts = []
   let i = 0
-  while (i = facts.length) {
+  while (i < facts.length) {
     shoutedFacts.push(`${facts}!!!`)
     i++
   }
