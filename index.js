141d4eadbba89b218c47fafb0bc02f62a914a6e3
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-11-28 01:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts(facts) {
   const shoutedFacts = []
   let i = 0
   while (i < facts.length) {
-    shoutedFacts.push(`${facts}!!!`)
+    shoutedFacts.push(`${facts[i]}!!!`)
     i++
   }
   return shoutedFacts
