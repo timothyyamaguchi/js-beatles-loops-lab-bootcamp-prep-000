794cbb9830624bf9a566f4ab89602dab9d201548
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-11-28 00:42:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function theBeatlesPlay(array, array1) {
   var musicians = []
   for (let i = 0; i < 10; i++) {
-    array.push('${array} plays ${array1}')
+    return array.push('${array} plays ${array1}')
   }
-  return array
 }
